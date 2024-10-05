e8d5dc4d03ec5bd43be52f758b31c8bbe16acc0a
yhwancha <chayounghwan0615@gmail.com>
2024-10-04 21:31:01 -0400

    refactor

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,6 @@
 import "./css/App.css";
 import React from "react";
-import Toolbar from "./components/Toolbar/Toolbar";
-import MapBoxComponent from "./components/Map/MapBoxComponent";
+
 import AppController from "./components/AppController";
 
 function App() {
