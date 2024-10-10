b7c164d5b0bfb87226ed133eb74f48bbff0d353c
yhwancha <chayounghwan0615@gmail.com>
2024-10-10 18:13:34 -0400

    feat: add swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API documentation using Swagger')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document); // Swagger UI가 'http://localhost:3001/api-docs'에 표시됨
+
+  await app.listen(3001);
 }
 bootstrap();
