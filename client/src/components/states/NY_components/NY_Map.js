c1ec7a9ae5497a461c4506f25f93cdb62d678499
ryangjinlee <ryangjin.lee@stonybrook.edu>
2024-10-11 04:00:02 -0400

    Bug Fix

--- client/src/components/states/NY_components/NY_Map.js
+++ client/src/components/states/NY_components/NY_Map.js
@@ -80,89 +80,336 @@ const NY_Map = (props) => {
             "match",
             ["get", "name"],
 
-            districtId.toString(), "black",
-            "orange"
-          ])
+            districtId.toString(),
+            "black",
+            "orange",
+          ]);
           break;
 
         case "MMD2":
           setLayerOption([
             "match",
             ["get", "name"],
-            districtId.toString(), "black",
-            (districtId === 9 || districtId === 10)
-              ? (districtId + 2).toString()
-              : (districtId === 11 || districtId === 12)
-                ? (districtId - 2).toString()
-                : (districtId === 26)
-                  ? (districtId - 3).toString()
-                  : (districtId === 23)
-                    ? (districtId + 3).toString()
-                    : (districtId === 25)
-                      ? (districtId - 1).toString()
-                      : (districtId === 24)
-                        ? (districtId + 1).toString()
-                        : (districtId % 2 === 0)
-                          ? (districtId - 1).toString()
-                          : (districtId + 1).toString(),
+            districtId.toString(),
             "black",
-            "orange"
+            districtId === 9 || districtId === 10
+              ? (districtId + 2).toString()
+              : districtId === 11 || districtId === 12
+              ? (districtId - 2).toString()
+              : districtId === 26
+              ? (districtId - 3).toString()
+              : districtId === 23
+              ? (districtId + 3).toString()
+              : districtId === 25
+              ? (districtId - 1).toString()
+              : districtId === 24
+              ? (districtId + 1).toString()
+              : districtId % 2 === 0
+              ? (districtId - 1).toString()
+              : (districtId + 1).toString(),
+            "black",
+            "orange",
           ]);
           break;
 
         case "MMD3":
-          setLayerOption([
-            "match",
-            ["get", "name"],
-            districtId.toString(), "black",
-            (districtId + 1).toString(), "black",
-            (districtId + 2).toString(), "black",
-            "orange"
-          ]);
+          if (districtId >= 1 && districtId <= 3) {
+            setLayerOption([
+              "match",
+              ["get", "name"],
+              "1",
+              "blue",
+              "2",
+              "blue",
+              "3",
+              "blue",
+              "orange",
+            ]);
+          } else if (districtId >= 4 && districtId <= 6) {
+            setLayerOption([
+              "match",
+              ["get", "name"],
+              "4",
+              "blue",
+              "5",
+              "blue",
+              "6",
+              "blue",
+              "orange",
+            ]);
+          } else if (districtId >= 7 && districtId <= 9) {
+            setLayerOption([
+              "match",
+              ["get", "name"],
+              "7",
+              "blue",
+              "8",
+              "blue",
+              "9",
+              "blue",
+              "orange",
+            ]);
+          } else if (districtId >= 10 && districtId <= 12) {
+            setLayerOption([
+              "match",
+              ["get", "name"],
+              "10",
+              "blue",
+              "11",
+              "blue",
+              "12",
+              "blue",
+              "orange",
+            ]);
+          } else if (districtId >= 13 && districtId <= 15) {
+            setLayerOption([
+              "match",
+              ["get", "name"],
+              "13",
+              "blue",
+              "14",
+              "blue",
+              "15",
+              "blue",
+              "orange",
+            ]);
+          } else if (districtId >= 16 && districtId <= 18) {
+            setLayerOption([
+              "match",
+              ["get", "name"],
+              "16",
+              "blue",
+              "17",
+              "blue",
+              "18",
+              "blue",
+              "orange",
+            ]);
+          } else if (districtId >= 19 && districtId <= 21) {
+            setLayerOption([
+              "match",
+              ["get", "name"],
+              "19",
+              "blue",
+              "20",
+              "blue",
+              "21",
+              "blue",
+              "orange",
+            ]);
+          } else if (
+            districtId === 22 ||
+            districtId === 24 ||
+            districtId === 25
+          ) {
+            setLayerOption([
+              "match",
+              ["get", "name"],
+              "22",
+              "blue",
+              "24",
+              "blue",
+              "25",
+              "blue",
+              "orange",
+            ]);
+          } else if (districtId >= 23 && districtId <= 26) {
+            setLayerOption([
+              "match",
+              ["get", "name"],
+              "23",
+              "blue",
+              "26",
+              "blue",
+              "orange",
+            ]);
+          }
           break;
 
         case "MMD4":
           setLayerOption([
             "match",
             ["get", "name"],
-            districtId.toString(), "black",
-            (districtId === 1 || districtId === 5 || districtId === 9 || districtId === 13 || districtId === 17)
-              ? [(districtId + 1).toString(), (districtId + 2).toString(), (districtId + 3).toString()]
-              : (districtId === 2 || districtId === 6 || districtId === 10 || districtId === 14 || districtId === 18)
-                ? [(districtId - 1).toString(), (districtId + 1).toString(), (districtId + 2).toString()]
-                : (districtId === 3 || districtId === 7 || districtId === 11 || districtId === 15 || districtId === 19)
-                  ? [(districtId - 1).toString(), (districtId + 1).toString(), (districtId - 2).toString()]
-                  : (districtId === 4 || districtId === 8 || districtId === 12 || districtId === 16 || districtId === 20)
-                    ? [(districtId - 1).toString(), (districtId - 2).toString(), (districtId - 3).toString()]
-                    : (districtId === 21)
-                      ? [(districtId + 1).toString(), (districtId + 3).toString(), (districtId + 4).toString()]
-                      : (districtId === 22)
-                        ? [(districtId - 1).toString(), (districtId + 2).toString(), (districtId + 3).toString()]
-                        : (districtId === 23)
-                          ? [(districtId + 3).toString()]
-                          : (districtId === 24)
-                            ? [(districtId - 3).toString(), (districtId - 2).toString(), (districtId + 1).toString()]
-                            : (districtId === 25)
-                              ? [(districtId - 4).toString(), (districtId - 3).toString(), (districtId - 1).toString()]
-                              : (districtId === 26)
-                                ? [(districtId - 3).toString()]
-                                : [(districtId + 1).toString(), (districtId + 2).toString(), (districtId + 3).toString()],
+            districtId.toString(),
             "black",
-            "orange"
+            districtId === 1 ||
+            districtId === 5 ||
+            districtId === 9 ||
+            districtId === 13 ||
+            districtId === 17
+              ? [
+                  (districtId + 1).toString(),
+                  (districtId + 2).toString(),
+                  (districtId + 3).toString(),
+                ]
+              : districtId === 2 ||
+                districtId === 6 ||
+                districtId === 10 ||
+                districtId === 14 ||
+                districtId === 18
+              ? [
+                  (districtId - 1).toString(),
+                  (districtId + 1).toString(),
+                  (districtId + 2).toString(),
+                ]
+              : districtId === 3 ||
+                districtId === 7 ||
+                districtId === 11 ||
+                districtId === 15 ||
+                districtId === 19
+              ? [
+                  (districtId - 1).toString(),
+                  (districtId + 1).toString(),
+                  (districtId - 2).toString(),
+                ]
+              : districtId === 4 ||
+                districtId === 8 ||
+                districtId === 12 ||
+                districtId === 16 ||
+                districtId === 20
+              ? [
+                  (districtId - 1).toString(),
+                  (districtId - 2).toString(),
+                  (districtId - 3).toString(),
+                ]
+              : districtId === 21
+              ? [
+                  (districtId + 1).toString(),
+                  (districtId + 3).toString(),
+                  (districtId + 4).toString(),
+                ]
+              : districtId === 22
+              ? [
+                  (districtId - 1).toString(),
+                  (districtId + 2).toString(),
+                  (districtId + 3).toString(),
+                ]
+              : districtId === 23
+              ? [(districtId + 3).toString()]
+              : districtId === 24
+              ? [
+                  (districtId - 3).toString(),
+                  (districtId - 2).toString(),
+                  (districtId + 1).toString(),
+                ]
+              : districtId === 25
+              ? [
+                  (districtId - 4).toString(),
+                  (districtId - 3).toString(),
+                  (districtId - 1).toString(),
+                ]
+              : districtId === 26
+              ? [(districtId - 3).toString()]
+              : [
+                  (districtId + 1).toString(),
+                  (districtId + 2).toString(),
+                  (districtId + 3).toString(),
+                ],
+            "black",
+            "orange",
           ]);
           break;
 
         case "MMD5":
-          setLayerOption([
-            "match",
-            ["get", "name"],
-            districtId.toString(), "black",
-            (districtId + 1).toString(), "black",
-            (districtId + 2).toString(), "black",
-            (districtId + 3).toString(), "black",
-            (districtId + 4).toString(), "black",
-            "orange"
-          ]);
+          if (districtId >= 1 && districtId <= 5) {
+            setLayerOption([
+              "match",
+              ["get", "name"],
+              "1",
+              "blue",
+              "2",
+              "blue",
+              "3",
+              "blue",
+              "4",
+              "blue",
+              "5",
+              "blue",
+              "orange",
+            ]);
+          } else if (
+            (districtId >= 6 && districtId <= 9) ||
+            districtId === 11
+          ) {
+            setLayerOption([
+              "match",
+              ["get", "name"],
+              "6",
+              "blue",
+              "7",
+              "blue",
+              "8",
+              "blue",
+              "9",
+              "blue",
+              "11",
+              "blue",
+              "orange",
+            ]);
+          } else if (
+            (districtId >= 12 && districtId <= 15) ||
+            districtId === 10
+          ) {
+            setLayerOption([
+              "match",
+              ["get", "name"],
+              "10",
+              "blue",
+              "12",
+              "blue",
+              "13",
+              "blue",
+              "14",
+              "blue",
+              "15",
+              "blue",
+              "orange",
+            ]);
+          } else if (districtId >= 16 && districtId <= 20) {
+            setLayerOption([
+              "match",
+              ["get", "name"],
+              "16",
+              "blue",
+              "17",
+              "blue",
+              "18",
+              "blue",
+              "19",
+              "blue",
+              "20",
+              "blue",
+              "orange",
+            ]);
+          } else if (
+            (districtId >= 21 && districtId <= 22) ||
+            districtId === 24 ||
+            districtId === 25
+          ) {
+            setLayerOption([
+              "match",
+              ["get", "name"],
+              "21",
+              "blue",
+              "22",
+              "blue",
+              "24",
+              "blue",
+              "25",
+              "blue",
+              "orange",
+            ]);
+          } else if (districtId === 23 || districtId === 26) {
+            setLayerOption([
+              "match",
+              ["get", "name"],
+              "23",
+              "blue",
+              "26",
+              "blue",
+              "orange",
+            ]);
+          }
           break;
 
         default:
@@ -171,8 +418,7 @@ const NY_Map = (props) => {
     }
   };
 
-  useEffect(() => {
-  }, [layerOption]);
+  useEffect(() => {}, [layerOption]);
 
   const handleViewStateChange = (newViewState) => {
     setViewState(newViewState); // Update view state to handle dragging and zooming
