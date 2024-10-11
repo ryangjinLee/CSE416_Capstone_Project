093c071fd0291ea9469e75f7de45dd1ba6d6bcdd
ryangjinlee <ryangjin.lee@stonybrook.edu>
2024-10-11 05:19:35 -0400

    Fixed WhiskerPlot Bug

--- client/src/components/states/NY_components/NY_MMD.js
+++ client/src/components/states/NY_components/NY_MMD.js
@@ -1,53 +1,33 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { AgCharts } from "ag-charts-react";
 import "ag-charts-enterprise";
-import { getData } from "../../../api";
+import ny_mmd_2_data from "../../../data/NY/box_mmd_2.json";
+import ny_mmd_3_data from "../../../data/NY/box_mmd_3.json";
+import ny_mmd_4_data from "../../../data/NY/box_mmd_4.json";
+import ny_mmd_5_data from "../../../data/NY/box_mmd_5.json";
 
 const NY_MMD = () => {
   const [selectedOptionCompare, setSelectedOptionCompare] = useState("MMD2");
-  const [ny_mmd_2, setNy_mmd_2] = useState({});
-  const [ny_mmd_3, setNy_mmd_3] = useState({});
-  const [ny_mmd_4, setNy_mmd_4] = useState({});
-  const [ny_mmd_5, setNy_mmd_5] = useState({});
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const mmd2 = await getData('/boxplot/ny/mmd2');
-        setNy_mmd_2(mmd2);
-        const mmd3 = await getData('/boxplot/ny/mmd3');
-        setNy_mmd_3(mmd3);
-        const mmd4 = await getData('/boxplot/ny/mmd4');
-        setNy_mmd_4(mmd4);
-        const mmd5 = await getData('/boxplot/ny/mmd5');
-        setNy_mmd_5(mmd5);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   const handleSelectChangeCompare = (event) => {
     setSelectedOptionCompare(event.target.value);
   };
 
-  const MMD2_total = ny_mmd_2.total
-  const MMD2_Democratic = ny_mmd_2.democratic
-  const MMD2_Republican = ny_mmd_2.republican
+  const MMD2_total = ny_mmd_2_data.total;
+  const MMD2_Democratic = ny_mmd_2_data.democratic;
+  const MMD2_Republican = ny_mmd_2_data.republican;
 
-  const MMD3_total = ny_mmd_3.total
-  const MMD3_Democratic = ny_mmd_3.democratic
-  const MMD3_Republican = ny_mmd_3.republican
+  const MMD3_total = ny_mmd_3_data.total;
+  const MMD3_Democratic = ny_mmd_3_data.democratic;
+  const MMD3_Republican = ny_mmd_3_data.republican;
 
-  const MMD4_total = ny_mmd_4.total
-  const MMD4_Democratic = ny_mmd_4.democratic
-  const MMD4_Republican = ny_mmd_4.republican
+  const MMD4_total = ny_mmd_4_data.total;
+  const MMD4_Democratic = ny_mmd_4_data.democratic;
+  const MMD4_Republican = ny_mmd_4_data.republican;
 
-  const MMD5_total = ny_mmd_5.total
-  const MMD5_Democratic = ny_mmd_5.democratic
-  const MMD5_Republican = ny_mmd_5.republican
+  const MMD5_total = ny_mmd_5_data.total;
+  const MMD5_Democratic = ny_mmd_5_data.democratic;
+  const MMD5_Republican = ny_mmd_5_data.republican;
 
   const [options, setOptions] = useState({
     title: {
