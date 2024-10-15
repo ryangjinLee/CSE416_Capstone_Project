6b08f366bfc6f4a5d04f12bb4ccdc020bf9632d3
yhwancha <chayounghwan0615@gmail.com>
2024-10-15 16:50:00 -0400

    fix: apply mongoDBservice

--- server/src/party/party.module.ts
+++ server/src/party/party.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PartyService } from './party.service';
 import { PartyController } from './party.controller';
+import { MongoDBService } from '../mongo-db/mongo-db.service';
 
 @Module({
   controllers: [PartyController],
-  providers: [PartyService],
+  providers: [PartyService, MongoDBService],
 })
 export class PartyModule {}
